40e75180d61eb9f1a8306f91887b626529be7cf2
mbashia <mbashiavictor@gmail.com>
2023-02-13 09:42:30 +0300

    second commit

--- birthday_paradox.py
+++ birthday_paradox.py
@@ -1,1 +1,6 @@
-
+# the birthday paradox, also called the birthday problem, is the surprisingly high probability that two
+# people will have the same birthday even in a small group of people. In a group of 70 people, there's
+# a 99.9 percent chance of two people having a matching birthday. But even in a group as small as 23 people, there's
+# 50 percent chance of a matching birthday.This program performs several probability experiments to determine the
+# percentages for groups of different sizes. We call these types of experiments,
+# in which we conuct multiple random trials to undersand the likely outcomes, Monte Carlo experiments
